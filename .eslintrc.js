dad20bf9b91405d1baf795b870347330c9cddaef
Forest <767425412lin@gmail.com>
2023-12-06 15:16:40 +0800

    initial project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    root: true,
+    extends: ['@react-native', 'plugin:prettier/recommended', 'prettier/react'],
+    rules: {
+        'no-shadow': 0,
+        'no-bitwise': 0,
+        'react-native/no-inline-styles': 0,
+        'no-restricted-imports': [
+            'error',
+            {
+                paths: [],
+            },
+        ],
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector:
+                    "ImportDeclaration[source.value='react-native'] > ImportSpecifier[imported.name=/Touchable\\w*/]",
+                message: 'Use Pressable instead',
+            },
+            // {
+            //   selector:
+            //     "ImportDeclaration[source.value='react-native'] > ImportSpecifier[imported.name='Image']",
+            //   message: 'Use FastImage from react-native-fast-image instead',
+            // },
+        ],
+    },
+};
