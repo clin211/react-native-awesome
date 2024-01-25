9fee476cdcc79bd911260d48bacdd91c057bc698
Forest <767425412lin@gmail.com>
2024-01-25 14:15:08 +0800

    feat: 添加icons

--- src/screens/UserCenter.tsx
+++ src/screens/UserCenter.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ScrollView, Text } from 'react-native';
-
-const Notice = () => {
+import Icon from 'react-native-vector-icons/FontAwesome';
+const UserCenter = () => {
     return (
         <ScrollView>
             <Text>Notice</Text>
+            <Icon name="rocket" size={30} color="#900" />
         </ScrollView>
     );
 };
 
-export default Notice;
+export default UserCenter;
