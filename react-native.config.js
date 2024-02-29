00922ad73947215e24b8e0ebf7e50b90d017df43
Forest <767425412lin@gmail.com>
2024-02-29 18:56:37 +0800

    feat: 添加字体

---
+++ react-native.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {},
+    },
+    assets: [
+        './src/assets/fonts/Manrope',
+        './src/assets/fonts/Montserrat',
+        './src/assets/fonts/Noto_Sans_SC',
+        './src/assets/fonts/Readex_Pro',
+        './src/assets/fonts/Roboto',
+    ],
+};
