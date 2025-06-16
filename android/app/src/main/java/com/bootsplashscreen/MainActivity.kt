b866f0c6270587a5b4a79daf72bd76ef02a20ef5
Forest <767425412lin@gmail.com>
2025-06-16 18:17:47 +0800

    feat: 启动屏基础版实现

--- android/app/src/main/java/com/bootsplashscreen/MainActivity.kt
+++ android/app/src/main/java/com/bootsplashscreen/MainActivity.kt
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
 
 class MainActivity : ReactActivity() {
 
@@ -18,5 +20,10 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+          DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ 初始化启动屏
+    super.onCreate(savedInstanceState) // 注意：如果使用 react-native-screens，这里应该是 super.onCreate(null)
+  }
 }
--- ios/BootSplashScreen/AppDelegate.swift
+++ ios/BootSplashScreen/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import RNBootSplash
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -44,5 +45,11 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 #else
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
+  }
+
+    // ⬇️ 重写此方法
+  override func customize(_ rootView: RCTRootView) {
+    super.customize(rootView)
+    RNBootSplash.initWithStoryboard("BootSplash", rootView: rootView) // ⬅️ 初始化启动屏
   }
 }
